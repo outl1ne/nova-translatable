98254d0638c74c72059dda7baa26ba2eee563cbe
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-12-20 15:45:12 +0200

    Initial macro test

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace OptimistDigital\MultiselectField;
+namespace OptimistDigital\NovaTranslatable;
 
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Fields\Field;
 
 class FieldServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,19 @@ class FieldServiceProvider extends ServiceProvider
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('translatable-field', __DIR__ . '/../dist/js/translatable-field.js');
+        });
+
+        // Register macro
+        ddd('macro!?');
+        Field::macro('translatable', function ($locales = []) {
+            $this->withMeta([
+                'translatable' => [
+                    'original_component' => $this->component,
+                    'locales' => $locales,
+                ],
+            ]);
+
+            $this->component = 'translatable-field';
         });
     }
 
