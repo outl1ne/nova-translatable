6772daae7d8d3e1048f5d245ee93aa493d681a0d
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-12-30 15:51:29 +0200

    Fix Index field

---
+++ resources/js/components/IndexField.vue
@@ -0,0 +1,16 @@
+<template>
+  <component
+    :is="'index-' + field.translatable.original_component"
+    :field="{ ...field, value: this.value[activeLocale] }"
+    :resource-name="resourceName"
+  ></component>
+</template>
+
+<script>
+import TranslatableField from '../mixins/TranslatableField';
+
+export default {
+  mixins: [TranslatableField],
+  props: ['resourceName', 'resourceId', 'resource', 'field'],
+};
+</script>
--- resources/js/translatable-field.js
+++ resources/js/translatable-field.js
@@ -1,7 +1,9 @@
 import FormField from './components/FormField';
 import DetailField from './components/DetailField';
+import IndexField from './components/IndexField';
 
 Nova.booting((Vue, router, store) => {
+  Vue.component('index-translatable-field', IndexField);
   Vue.component('form-translatable-field', FormField);
   Vue.component('detail-translatable-field', DetailField);
 });
