9daa814b42821a5896b336ebc192637662cce9a9
Nicolas Giraud <contact@ngiraud.me>
2020-01-22 12:17:41 +0100

    Set the active locale to Nova.config.locale and sort the tabs to display the active locale first

--- resources/js/components/LocaleTabs.vue
+++ resources/js/components/LocaleTabs.vue
@@ -2,7 +2,7 @@
   <div class="flex select-none" :class="wrapperClasses">
     <div class="ml-auto">
       <a
-        v-for="locale in locales"
+        v-for="locale in sortedLocales"
         :key="locale.key"
         class="ml-3 cursor-pointer font-bold text-80 text-sm"
         :class="{
@@ -26,6 +26,15 @@ export default {
       if (this.detail) return ['pt-4'];
       return ['pt-4', 'px-8'];
     },
+    sortedLocales() {
+      const novaLocale = _.find(this.locales, ['key', Nova.config.locale])
+
+      if(!novaLocale) {
+        return this.locales
+      }
+
+      return [novaLocale, ...this.locales.filter(({key}) => key !== Nova.config.locale)];
+    }
   },
   methods: {
     hasError(locale) {
