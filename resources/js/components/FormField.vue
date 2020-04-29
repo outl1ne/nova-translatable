dcc1373b8742a218551c201c4a764839da25d147
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-04-29 11:55:29 +0300

    Style fixes

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -75,14 +75,13 @@ export default {
 
           const data = {};
           const originalAttribute = this.field.translatable.original_attribute;
-          
+
           for (const locale of this.locales) {
             const tempFormData = new FormData();
             const field = this.fields[locale.key];
             field.fill(tempFormData);
 
-            const formDataKeys = Array.from(tempFormData.keys());            
-
+            const formDataKeys = Array.from(tempFormData.keys());
             for (const rawKey of formDataKeys) {
               const [key, value] = this.getKeyAndValue(rawKey, locale, tempFormData);
 
