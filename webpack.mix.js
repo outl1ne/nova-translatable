424a8c81ec5fc42bb52033c07eadf19ce222c563
Tarvo R. <TarvoReinpalu@gmail.com>
2024-12-18 15:36:59 +0200

    Nova v5 build process fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,10 @@
 let mix = require('laravel-mix');
 
+mix.extend('nova', new require('laravel-nova-devtool'));
+
 mix
   .setPublicPath('dist')
   .js('./resources/js/translatable-field.js', 'nova-translatable.js')
   .js('./resources/js/locale-select-field.js', 'nova-translatable-select-field.js')
   .vue({ version: 3 })
-  .webpackConfig({
-    externals: {
-      vue: 'Vue',
-      'laravel-nova': 'LaravelNova'
-    },
-    output: {
-      uniqueName: 'outl1ne/nova-translatable',
-    },
-  })
+  .nova('outl1ne/nova-translatable');
